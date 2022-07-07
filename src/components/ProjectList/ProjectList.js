60b3968f830a16a99f1f40974a4868bb12a887a4
Alexis LAGODKA <alexis.lagodka@bethunebruay.fr>
2022-07-07 14:40:57 +0200

    add valid href

--- src/components/ProjectList/ProjectList.js
+++ src/components/ProjectList/ProjectList.js
@@ -36,7 +36,7 @@ export default function ProjectList({ in: inProp }) {
                 eiusmod tempor incididunt ut labore et dolore magna aliqua.
               </p>
               <div>
-                <a href="#">Learn more &#x2192;</a>
+                <a href="https://unsplash.com/">Learn more &#x2192;</a>
               </div>
             </ProjectCard>
             <ProjectCard picture="https://images.unsplash.com/photo-1589307004173-3c95204d00ee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80">
@@ -46,6 +46,9 @@ export default function ProjectList({ in: inProp }) {
                 eiusmod tempor incididunt ut labore et dolore magna aliqua.
               </p>
               <p>Learn more &#x2192;</p>
+              <div>
+                <a href="https://unsplash.com/">Learn more &#x2192;</a>
+              </div>
             </ProjectCard>
             <ProjectCard picture="https://images.unsplash.com/photo-1607457597191-8ed4e870ceca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80">
               <h2>ABC</h2>
@@ -53,7 +56,9 @@ export default function ProjectList({ in: inProp }) {
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua.
               </p>
-              <p>Learn more &#x2192;</p>
+              <div>
+                <a href="https://unsplash.com/">Learn more &#x2192;</a>
+              </div>
             </ProjectCard>
             <ProjectCard picture="https://images.unsplash.com/photo-1529641484336-ef35148bab06?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80">
               <h2>ABC</h2>
@@ -61,7 +66,9 @@ export default function ProjectList({ in: inProp }) {
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua.
               </p>
-              <p>Learn more &#x2192;</p>
+              <div>
+                <a href="https://unsplash.com/">Learn more &#x2192;</a>
+              </div>
             </ProjectCard>
             <ProjectCard picture="https://images.unsplash.com/photo-1604076913837-52ab5629fba9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80">
               <h2>ABC</h2>
@@ -69,6 +76,9 @@ export default function ProjectList({ in: inProp }) {
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua.
               </p>
+              <div>
+                <a href="https://unsplash.com/">Learn more &#x2192;</a>
+              </div>
             </ProjectCard>
           </div>
         </div>
